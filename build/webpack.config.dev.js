75ca248ff3199ab4cdaa2125d80b2971f6940d6f
yangfang@bignox.com <yangfang@bignox.com>
2019-11-09 14:51:20 +0800

    初步客户端打包配置

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,1 +1,26 @@
 // 初始webpack配置
+const path=require('path');
+const HtmlPlugin=require('html-webpack-plugin');
+module.exports={
+    mode:'development',
+    entry:{
+        app:path.join(__dirname,"../client/index.js"),
+    },
+    output:{
+        path:path.join(__dirname,'../dist'),
+        filename:'app.[hash].js',
+        publicPath:"/public"
+    },
+    module: {
+        rules: [
+          {
+            test: /.js$/,
+            loader: 'babel-loader',
+            exclude: path.join(__dirname, '../node_modules')
+          }
+        ]
+      },
+    plugins:[
+        new HtmlPlugin()
+    ]
+}
---
+++ client/app.js
@@ -0,0 +1,8 @@
+import React from 'React';
+export default class App extends React.Component{
+    render(){
+        return (
+            <div>hello</div>
+        )
+    }
+}
